d7176546eb44377ff4fb916c11390d0e08f5b99a
Chris Cornutt <enygma@phpdeveloper.org>
2019-03-03 16:05:31 -0600

    updating clock item for formatting

--- items/clock.py
+++ items/clock.py
@@ -1,4 +1,3 @@
-# from adventurelib import *
 from detail_item import *
 
 clock = Detail_Item('a clock', 'clock')
@@ -9,5 +8,4 @@ at least close to the correct time, but it may have slowed down over the years. 
 old clocks you know, this one has three hands rather than just two: an hour hand, a
 minute hand and a second hand. As far as you can tell it doesn't have any fancy internal
 workings you can pry at, but on the back, right behind the face, you do find three holes,
-as narrow as a coat hanger wire.
-"""
+as narrow as a coat hanger wire."""
