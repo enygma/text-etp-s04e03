45c2c26e0c062d708423c6c85e0e0d602eb94620
Chris Cornutt <enygma@phpdeveloper.org>
2019-03-03 16:07:04 -0600

    adding rile item to game room

--- rooms/game_room.py
+++ rooms/game_room.py
@@ -3,6 +3,7 @@ from detail_room import *
 from detail_item import *
 
 from items.clock import *
+from items.rifle import *
 
 game_room = Detail_Room("""
 The first thing that enters your head is that this is like an old-fashioned man cave. It's a
@@ -20,7 +21,6 @@ fire is a single chair with a small side table next to it.
 game_room.name = 'game_room'
 game_room.title = "The Game Room"
 
-rifle = Detail_Item('an old rifle', 'rifle')
 painting = Detail_Item('a large painting', 'painting')
 card_table = Detail_Item('a card table', 'card_table')
 chairs = Detail_Item('a few chairs', 'chairs')
