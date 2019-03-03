d1202dc2feea345a3644bce5964be0889d9c7807
Chris Cornutt <enygma@phpdeveloper.org>
2019-03-03 08:58:51 -0600

    adding the clock item

---
+++ items/clock.py
@@ -0,0 +1,13 @@
+# from adventurelib import *
+from detail_item import *
+
+clock = Detail_Item('a clock', 'clock')
+clock.takeable = False
+clock.description = """
+It isn't a grandfather clock, but it isn't much smaller than one. It's working, and is
+at least close to the correct time, but it may have slowed down over the years. Unlike most
+old clocks you know, this one has three hands rather than just two: an hour hand, a
+minute hand and a second hand. As far as you can tell it doesn't have any fancy internal
+workings you can pry at, but on the back, right behind the face, you do find three holes,
+as narrow as a coat hanger wire.
+"""
