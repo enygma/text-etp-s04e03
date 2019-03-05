9d2612c95d339256955cb0bd3bc946d5ff069519
Chris Cornutt <enygma@phpdeveloper.org>
2019-03-04 23:09:44 -0600

    adding defaults for the load/unload action methods

--- detail_item.py
+++ detail_item.py
@@ -3,8 +3,17 @@ from adventurelib import *
 class Detail_Item(Item):
     description = "It doesn't seem to be anything special."
     takeable = True
+
+    actions = []
     
     def __init__(self, name, *aliases, takeable=True):
+        self.actions = []
         self.takeable = takeable
         Item.__init__(self, name, *aliases)
 
+
+    def load_actions(self):
+        return True
+
+    def unload_actions(self):
+        return True
