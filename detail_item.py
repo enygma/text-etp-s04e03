bd1de7f8fbfdb252c517f43f32ae7d2c7282935f
Chris Cornutt <enygma@phpdeveloper.org>
2019-03-10 22:00:08 -0500

    making the "load actions" callable to reload them

--- detail_item.py
+++ detail_item.py
@@ -1,4 +1,6 @@
 from adventurelib import *
+from adventurelib import _register
+from adventurelib import commands
 
 class Detail_Item(Item):
     description = "It doesn't seem to be anything special."
@@ -13,6 +15,19 @@ class Detail_Item(Item):
 
 
     def load_actions(self):
+        actions = self.actions
+        if len(actions) > 0:
+            for action in actions:
+                # See if the command already exists
+                found = False
+                for command in commands:
+                    if command[0].orig_pattern == action['match']:
+                        found = True
+
+                if found == False:
+                    func = getattr(self, action['method'])
+                    _register(action['match'], func)
+
         return True
 
     def unload_actions(self):
