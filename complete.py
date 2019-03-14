a5c531ddb72f43539ac941e9361906777364b308
Chris Cornutt <enygma@phpdeveloper.org>
2019-03-13 21:53:15 -0500

    message shown on completion

---
+++ complete.py
@@ -0,0 +1,43 @@
+from colored import fg, bg, attr
+
+def finish():
+    print('finishing')
+    print("%s%sA tremendous, earthshaking gong sounds through the air. Then another. And another..%s" % (fg('white'), attr('bold'), attr('reset')))
+    print("""
+You have no idea where it's coming from; the clock doesn't have the inner workings to do
+this. Plus, what kind of clock gongs at 6:56? It keeps going. Filling the room. Then, another
+sound roars above it – a voice.
+
+“You are LATE!”
+
+It shrieks in your ears. You hear a gunshot and feel something zoom past your cheek. Out
+of instinct, you drop to the floor, and it's a good thing you do because you hear several
+more gunshots, all accompanied by this screaming, raving voice. “LATE! LATE!
+LAAAAAAAAAAAAAAA...”
+
+But before it can finish, it's gone. So are the gunshots, and so is the gong. It's all stopped.
+The only sound left is a rattle coming from the inside of the clock, and after a few seconds,
+that too subsides. You stand up and look at the clock face. It's frozen. 6:56 and zero
+seconds.
+
+You walk towards the doors and find that they now open when you push. Looks like you've
+indeed trapped the ghost, and now you're free. Well, you don't need to be told twice. That
+said, you take a moment to consider what to do with the possibly-possessed clock; in the
+end you pick it up and haul it out with you. Maybe you can burn it just to be safe.
+When you reach the front of the manor, you find a person waiting for you. Bill, you presume.
+He looks relieved to see you, and remarkably unconcerned about you dragging a huge
+antique out of his museum.
+
+“Good job,” he says. “I heard some noises that made me worry you weren't going to make
+it. Did I hear gunfire? Who was this ghost?”
+
+You tell him a bit about what you witnessed, and after a moment he nods.
+
+“Ah, Philip. Mr Culloden's hunting instructor. Interesting... we've had his chauffeur, his
+chef, and now his hunting teacher. It seems like people who worked for him are the
+common denominator. But to have so many ghosts, so many people with unfinished
+business, bent on revenge – it seems strange, doesn't it?”
+
+He frowns.
+“What went on in this house to leave behind so much hatred?”
+""")
